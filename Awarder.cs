66057327c7b84c91f4e2044eb41a25e5aa12f40f
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-03-17 14:30:08 -0500

    No music today :(
    Implement CalculateAward method to determine award based on points.

--- Awarder.cs
+++ Awarder.cs
@@ -2,4 +2,19 @@
 public class Awarder
 {
     // Add your code here
+    public static string CalculateAward(int points) {
+        if (points > 99) {
+            return "First Place";
+        }
+        else if (points > 49 && points < 100) {
+            return "Second Place";
+        }
+        else {
+            return "Participation Award";
+        }
+    }
 }
+
+//should create a method named CalculateAward that takes one parameter points and returns the string "First Place" when points are over 99
+//should return the string "Second Place" from CalculateAward when points are over 49 and under 100
+//should return the string "Participation Award" from CalculateAward for everything else
